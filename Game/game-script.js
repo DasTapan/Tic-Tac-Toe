ba6299a56a3ef4b66bbeba67b4b8c1612876af69
Das Tapan <tapands.das@gmail.com>
2022-10-12 15:16:19 +0530

    Show input on click

--- Game/game-script.js
+++ Game/game-script.js
@@ -18,6 +18,37 @@ const Player = (_name, _mark, _valid) => {
     }
 }
 
+const game = (() => {
+    let _validPlayer;
+    let _validPlayerMark;
+
+    const _validator = () => {
+        if (player1.getValidStatus() == true) _validPlayer = player1
+        else _validPlayer = player2
+
+        _validPlayerMark = _validPlayer.getMark();
+        return _validPlayerMark;
+    }
+
+    const getValidMark = () => {
+        return _validator();
+    }
+
+    return { getValidMark };
+})();
+
+const displayController = (() => {
+    const _render = (_cell, _mark) => {
+        _cell.textContent = _mark;
+    }
+
+    const displayInput = (cell,mark) => {
+        _render(cell,mark);
+    }
+
+    return { displayInput };
+})();
+
 const player1 = Player('player1', 'X', true);
 const player2 = Player('player2', 'X', false);
 
@@ -25,4 +56,6 @@ gameBoarDiv.addEventListener('click', (event) => {
     let targetedCell = event.target.closest('div');
     let targetedCellClass = targetedCell.classList[1];
     console.log(targetedCellClass);
+    let validMark = game.getValidMark();
+    displayController.displayInput(targetedCell, validMark);
 })
