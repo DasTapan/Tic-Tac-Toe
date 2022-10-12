a2b94515b5641a8fda685dd93a110938c40bbef5
Das Tapan <tapands.das@gmail.com>
2022-10-12 12:50:30 +0530

    Create Player factory function

--- Game/game-script.js
+++ Game/game-script.js
@@ -1,5 +1,28 @@
 const gameBoarDiv = document.querySelector('.game-board');
+
+const Player = (_name, _mark, _valid) => {
+    const getMark = () => _mark;
+    const getValidStatus = () => _valid;
+    const _setValidity = () => {
+        _valid = !_valid;
+    }
+
+    const toggleValidity = () => {
+        _setValidity();
+    }
+
+    return {
+        getMark,
+        getValidStatus,
+        toggleValidity
+    }
+}
+
+const player1 = Player('player1', 'X', true);
+const player2 = Player('player2', 'X', false);
+
 gameBoarDiv.addEventListener('click', (event) => {
     let targetedCell = event.target.closest('div');
-    console.log(targetedCell);
+    let targetedCellClass = targetedCell.classList[1];
+    console.log(targetedCellClass);
 })
